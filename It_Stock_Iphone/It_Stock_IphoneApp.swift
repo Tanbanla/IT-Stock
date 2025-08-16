626b907e1180839d2f0c81176a0f4b055dbc5677
Nguyen Duy Khanh <tungvipduong@gmail.com>
2025-08-16 08:29:16 +0700

    Initial Commit

---
+++ It_Stock_Iphone/It_Stock_IphoneApp.swift
@@ -0,0 +1,17 @@
+//
+//  It_Stock_IphoneApp.swift
+//  It_Stock_Iphone
+//
+//  Created by BIVNITMAC on 16/8/25.
+//
+
+import SwiftUI
+
+@main
+struct It_Stock_IphoneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ It_Stock_IphoneTests/It_Stock_IphoneTests.swift
@@ -0,0 +1,17 @@
+//
+//  It_Stock_IphoneTests.swift
+//  It_Stock_IphoneTests
+//
+//  Created by BIVNITMAC on 16/8/25.
+//
+
+import Testing
+@testable import It_Stock_Iphone
+
+struct It_Stock_IphoneTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
